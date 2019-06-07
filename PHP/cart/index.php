b2df7b13d7c6dced75e0172cdbd75ec431c57a84
Tho <thopn.hvcsnd@gmail.com>
2019-06-07 21:00:44 +0700

    Giỏ hàng trong PHP

---
+++ PHP/cart/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		td{
+			border: 1px solid blue;
+			padding: 10px;
+			text-align: center;
+		}
+		table{
+			margin:10px auto;
+			width: 80%;
+		}
+	</style>
+</head>
+<body>
+	<div style="text-align: center;font-weight: bold; font-size: 20px">
+		DỮ LIỆU TỪ TRANG CHỦ
+	</div>
+	<table>
+		<tr>
+			<td>
+				Sản phẩm A
+				<img src="#" width="100px" height="100px">
+				<a href="cart.php?modem=a">Thêm vào giỏ</a>
+			</td>
+			<td>
+				Sản phẩm B
+				<img src="#" width="100px" height="100px">
+				<a href="cart.php?modem=b">Thêm vào giỏ</a>
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				Sản phẩm C
+				<img src="#" width="100px" height="100px">
+				<a href="cart.php?modem=c">Thêm vào giỏ</a>
+			</td>
+			<td>
+				Sản phẩm D
+				<img src="#" width="100px" height="100px">
+				<a href="cart.php?modem=d">Thêm vào giỏ</a>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
---
+++ PHP/php5_oop/submit.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="run_point.php" method="post">
+		NHẬP TOẠ ĐỘ CHO ĐIỂM A:<br>
+		Mời nhập toạ độ X
+		<input type="text" name="txtX"><br>
+		Mời nhập toạ độ Y
+		<input type="text" name="txtY"><br>
+
+		<hr>
+		NHẬP TOẠ ĐỘ CHO ĐIỂM B:<br>
+		Mời nhập toạ độ X
+		<input type="text" name="txtXB"><br>
+		Mời nhập toạ độ Y
+		<input type="text" name="txtYB"><br>
+
+		<input type="submit" name="btnSubmit" value="Lấy toạ độ">
+	</form>
+	
+</body>
+</html>
